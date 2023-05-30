46ddd4f097204281f37736173e5c391ee30e3414
Glitchmin <adam.p.kulczycki@gmail.com>
2023-05-30 14:04:43 +0200

    height apogee detector parameter tweaking

--- arduino/ppga/params.h
+++ arduino/ppga/params.h
@@ -7,8 +7,8 @@ typedef int my_time_t;
 const int PAST_SAMPLE_COMPARED = 5;
 const int NEG_STREAK_REQ = 3;
 const altitude_t MIN_ALT = 5; //meter
-const altitude_t MAX_ALT_DIFF_REQ = 0.8; //meter
-const altitude_t MAX_ALT_DIFF_DEFINITE = 3; //meter
+const altitude_t MAX_ALT_DIFF_REQ = 0.2; //meter
+const altitude_t MAX_ALT_DIFF_DEFINITE = 1.0; //meter
 const int SAMPLES_IN_NOISE_FILTER = 5;
 
 #endif
